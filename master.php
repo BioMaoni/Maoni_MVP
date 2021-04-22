f673b604856e05c5c2bac39644ea0eb6d4926456
Jython-master <43645058+Jython-master@users.noreply.github.com>
2021-04-22 12:06:23 -0700

    New master

--- master.php
+++ master.php
@@ -8,14 +8,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
+    
     <title>Maoni</title>
     <link rel="icon" type="image/x-icon" href="../bootstrap/img/ICOMAONI.ico" />
     <link href="../bootstrap/img/ICOMAONI.png" rel="apple-touch-icon">
     <!-- Custom fonts for this template-->
     <link href="../bootstrap/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
+    <script type="text/javascript" src="https://prod-uk-a.online.tableau.com/javascripts/api/viz_v1.js"></script> 
     <!-- Custom styles for this template-->
     <link href="../bootstrap/css/sb-admin-2.min.css" rel="stylesheet">
 
