0628df8d6314a0a1989af8ce68b25dfeab647288
BioMaoni <bio.guerra@maoni.co.uk>
2021-04-26 18:04:40 -0300

    Adicionando novo layut sidebar

--- index.php
+++ index.php
@@ -19,6 +19,81 @@
     <!-- Custom styles for this template-->
     <link href="bootstrap/css/sb-admin-2.min.css" rel="stylesheet">
 
+    <script>
+        window['_fs_debug'] = false;
+        window['_fs_host'] = 'fullstory.com';
+        window['_fs_script'] = 'edge.fullstory.com/s/fs.js';
+        window['_fs_org'] = '12D2S5';
+        window['_fs_namespace'] = 'FS';
+        (function(m, n, e, t, l, o, g, y) {
+            if (e in m) {
+                if (m.console && m.console.log) {
+                    m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');
+                }
+                return;
+            }
+            g = m[e] = function(a, b, s) {
+                g.q ? g.q.push([a, b, s]) : g._api(a, b, s);
+            };
+            g.q = [];
+            o = n.createElement(t);
+            o.async = 1;
+            o.crossOrigin = 'anonymous';
+            o.src = 'https://' + _fs_script;
+            y = n.getElementsByTagName(t)[0];
+            y.parentNode.insertBefore(o, y);
+            g.identify = function(i, v, s) {
+                g(l, {
+                    uid: i
+                }, s);
+                if (v) g(l, v, s)
+            };
+            g.setUserVars = function(v, s) {
+                g(l, v, s)
+            };
+            g.event = function(i, v, s) {
+                g('event', {
+                    n: i,
+                    p: v
+                }, s)
+            };
+            g.anonymize = function() {
+                g.identify(!!0)
+            };
+            g.shutdown = function() {
+                g("rec", !1)
+            };
+            g.restart = function() {
+                g("rec", !0)
+            };
+            g.log = function(a, b) {
+                g("log", [a, b])
+            };
+            g.consent = function(a) {
+                g("consent", !arguments.length || a)
+            };
+            g.identifyAccount = function(i, v) {
+                o = 'account';
+                v = v || {};
+                v.acctId = i;
+                g(o, v)
+            };
+            g.clearUserCookie = function() {};
+            g.setVars = function(n, p) {
+                g('setVars', [n, p]);
+            };
+            g._w = {};
+            y = 'XMLHttpRequest';
+            g._w[y] = m[y];
+            y = 'fetch';
+            g._w[y] = m[y];
+            if (m[y]) m[y] = function() {
+                return g._w[y].apply(this, arguments)
+            };
+            g._v = "1.3.0";
+        })(window, document, window['_fs_namespace'], 'script', 'user');
+    </script>
+
 </head>
 
 <body id="page-top">
@@ -42,7 +117,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="" onclick="header()">
+                <a class="nav-link" href="index.php?par=dashboard" onclick="header()">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -69,18 +144,58 @@
                     <span>Meetings</span></a>
             </li>-->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+                <a class="nav-link collapsed" style="padding: .5rem 1rem;" href="#" data-toggle="collapse" data-target="#cross_topics" aria-expanded="true" aria-controls="collapsePages">
                     <i class="fas fa-fw fa-folder"></i>
                     <span>Cross-Functional</span>
                 </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Weekly Shake</h6>
-                        <a class="collapse-item" href="index.php?par=sentiment">Sentiment Over Time</a>
-                        <a class="collapse-item" href="index.php?par=tagged">Tagged Topics</a>
-                        <a class="collapse-item" href="index.php?par=key">Key Topics</a>
+                <div id="cross_topics" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                    <div class="collapse-inner" style="padding: 0rem;">
+                        <h1 class="collapse-header" style="padding: 0rem; font-size: .65rem; color: white;">Weekly Shake</h1>
+                        <a class="nav-link w-100 h-100" style="padding: .5rem 1rem;" href="">Historical Sentiment</a>
+                        <a class="nav-link w-100 h-100" style="padding: .5rem 1rem;" href="">Teams - Sentiment</a>
+                        <!-- estudar data-parent -->
+                        <a class="nav-link collapsed w-100 h-100" style="padding: .5rem 1rem;" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+                            <i class="fas fa-fw fa-folder"></i>
+                            <span>Key Topics</span>
+                        </a>
+                        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#cross_topics">
+                            <div class="bg-white collapse-inner rounded" style="padding: 0rem;">
+                                <h3 class="collapse-header">Sentiment</h3>
+                                <a class="collapse-item" href="index.php?par=sentiment">People</a>
+                                <a class="collapse-item" href="index.php?par=tagged">Onboarding</a>
+                                <a class="collapse-item" href="index.php?par=key">Scalling</a>
+                                <a class="collapse-item" href="index.php?par=key">Technology</a>
+                                <a class="collapse-item" href="index.php?par=key">Operations</a>
+                            </div>
+                        </div>
+                        <a class="nav-link collapsed w-100 h-100" style="padding: .5rem 1rem;" href="#" data-toggle="collapse" data-target="#collapsePagess" aria-expanded="true" aria-controls="collapsePages">
+                            <i class="fas fa-fw fa-folder"></i>
+                            <span>Tagged Topics</span>
+                        </a>
+                        <!-- data-parent="#accordionSidebar" -->
+                        <div id="collapsePagess" class="collapse" aria-labelledby="headingPages" data-parent="#cross_topics">
+                            <div class="bg-white collapse-inner rounded" style="padding: 0rem;">
+                                <h3 class="collapse-header">Sentiment</h3>
+                                <a class="collapse-item" href="index.php?par=sentiment">Global Goals</a>
+                                <a class="collapse-item" href="index.php?par=tagged">Countries</a>
+                                <a class="collapse-item" href="index.php?par=key">Product Offering</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
+            </li>
+
+            <hr class="sidebar-divider">
+
+            <div class="sidebar-heading">
+                Others
+            </div>
+
+            <li class="nav-item">
+                <a class="nav-link" style="padding: .5rem 1rem;" href="#">
+                    <i class="fas fa-fw fa-folder"></i>
+                    <span>Remote Work Playbook</span>
+                </a>
             </li>
 
             <!-- Nav Item - Pages Collapse Menu -->
