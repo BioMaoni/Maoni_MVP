136ac2a829052ec00dfd1960ea002baf103d281f
BioMaoni <bio.guerra@maoni.co.uk>
2021-05-04 08:38:47 -0300

    Retirando F

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ require_once "controller/user/loggedIn.php";
     <meta http-equiv="cache-control" content="no-cache" />
     <meta http-equiv="expires" content="0" />
     <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
-    <meta http-equiv="pragma" content="no-cache" />F
+    <meta http-equiv="pragma" content="no-cache" />
 
     <title>Maoni</title>
     <link rel="icon" type="image/x-icon" href="bootstrap/img/ICOMAONI.ico" />
